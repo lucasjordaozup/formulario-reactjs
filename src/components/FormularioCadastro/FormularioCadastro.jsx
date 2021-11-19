1b22ccd48175289d23e08134d471a3cfab18902a
LucasWilliam12 <lucas.jordao@zup.com.br>
2021-11-19 00:22:17 -0300

    Instalando o material ui e criando um botão

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 export default function FormularioCadastro(){
     return (
         <form>
+            <label htmlFor="">Nome</label>
             <input type="text" />
+            
+            <label htmlFor="">Sobrenome</label>
+            <input type="text" />
+            
+            <label htmlFor="">CPF</label>
+            <input type="text" />
+
+            <label htmlFor="">Promoções</label>
+            <input type="checkbox" />
+            
+            <label htmlFor="">Novidades</label>
+            <input type="checkbox" />
+
+            <Button type="submit" variant="contained" color="primary">Cadastrar</Button>            
+
         </form>
     );
 }
